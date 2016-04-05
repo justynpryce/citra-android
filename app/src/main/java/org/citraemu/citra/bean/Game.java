08a2bd1875b877ad5447b539e72f1109338f2351
Kloen <jlcc20@gmail.com>
2016-04-05 15:25:18 +0200

    Base game_list components

---
+++ app/src/main/java/org/citraemu/citra/bean/Game.java
@@ -0,0 +1,34 @@
+package org.citraemu.citra.bean;
+
+import android.graphics.drawable.Drawable;
+
+public class Game {
+
+    private Drawable image;
+    private String title;
+    private double size;
+
+    public Drawable getImage() {
+        return image;
+    }
+
+    public void setImage(Drawable image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+}
